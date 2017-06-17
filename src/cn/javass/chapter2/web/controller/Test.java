eed2a04276dc9d2d6c0ed372bd10efbe7aca2937
zhangwenbing1989 <zhangwenbing1989@163.com>
2017-06-17 14:48:20 +0800

    add test

---
+++ src/cn/javass/chapter2/web/controller/Test.java
@@ -0,0 +1,17 @@
+
+  	/*@(#)Test.java   2017-6-17 
+     * Copy Right 2017 Bank of Communications Co.Ltd.
+     * All Copyright Reserved
+     */
+  
+package cn.javass.chapter2.web.controller;
+
+
+  	
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world.");
+    }
+
+}
